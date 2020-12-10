bfab5c1099c2db18372857c15f6af8314ff42e44
“hellobkrishna” <bablookumarsah333@gmail.com>
2020-12-10 08:50:27 +0530

    creates model

--- coreapp/admin.py
+++ coreapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Pizza, Type, Size, Topping
+
+admin.site.register(Pizza)
+admin.site.register(Type)
+admin.site.register(Size)
+admin.site.register(Topping)
--- coreapp/models.py
+++ coreapp/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+class Size(models.Model):
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=200)
+    
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Topping(models.Model):
+    name = models.CharField(max_length=200)
+    pizza = models.ManyToManyField(Pizza)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
